73595a5f2f636f2e540529531b2d45f5a3cd59f3
oNavShaHo <106837111+oNavShaHo@users.noreply.github.com>
2023-07-16 01:51:08 +0530

    page.js added

---
+++ src/Components/page.js
@@ -0,0 +1,108 @@
+"use client";
+
+import { useState, useRef } from "react";
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+  Typography,
+  IconButton,
+  Button,
+} from "@material-tailwind/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
+function Icon({ id, open }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={`${
+        id === open ? "rotate-180" : ""
+      } h-5 w-5 transition-transform`}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+    </svg>
+  );
+}
+
+const Questions = [
+  {
+    id: 1,
+    question: "What is Lorem ipsum?",
+    answer:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+  },
+  {
+    id: 2,
+    question: "What is Lorem ipsum?",
+    answer:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+  },
+  {
+    id: 3,
+    question: "What is Lorem ipsum?",
+    answer:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+  },
+  {
+    id: 4,
+    question: "What is Lorem ipsum?",
+    answer:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+  },
+  {
+    id: 5,
+    question: "What is Lorem ipsum?",
+    answer:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+  },
+];
+
+export default function Home() {
+  const [open, setOpen] = useState(false);
+  const handleDrawer = () => setOpen(!open);
+  const [openAccordion, setOpenAccordion] = useState(0);
+  const clickRef = useRef();
+
+  const handleAccordion = (value) => {
+    setOpenAccordion(openAccordion === value ? 0 : value);
+  };
+
+  return (
+    <>
+      <Button onClick={handleDrawer}>Open Drawer</Button>
+      <div
+        ref={clickRef}
+        className={`${
+          open ? "translate-x-0" : "translate-x-[100vw]"
+        } fixed sm:top-2 sm:right-2 z-40 ease-in-out duration-300 sm:w-[95vw] md:w-[50vw] lg:w-[30vw] m-2 sm:m-3 p-5 rounded-lg backdrop-blur-md h-full`}
+      >
+        <div className="mb-2 flex items-center justify-between">
+          <Typography variant="h5" color="black">
+            General FAQs
+          </Typography>
+          <IconButton variant="text" color="blue-gray" onClick={handleDrawer}>
+            <XMarkIcon strokeWidth={2} className="h-5 w-5" />
+          </IconButton>
+        </div>
+        <div className="flex flex-col items-center justify-start p-3 h-full overflow-y-scroll">
+          {Questions.map((question) => (
+            <Accordion
+              key={question.id}
+              open={openAccordion === question.id}
+              icon={<Icon id={question.id} open={openAccordion} />}
+            >
+              <AccordionHeader onClick={() => handleAccordion(question.id)}>
+                {question.question}
+              </AccordionHeader>
+              <AccordionBody>{question.answer}</AccordionBody>
+            </Accordion>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
