56a13f3288e39e1d54808853a3334aa5382ae13f
oNavShaHo <106837111+oNavShaHo@users.noreply.github.com>
2023-07-21 02:40:34 +0530

    phone screen blur

--- src/Components/page.js
+++ src/Components/page.js
@@ -86,7 +86,7 @@ export default function Home() {
         ref={clickRef}
         className={`${
           open ? "translate-x-0" : "translate-x-[100vw]"
-        } fixed sm:top-2 right-2 z-40 ease-in-out duration-300 sm:w-[95vw] md:w-[50vw] lg:w-[30vw] m-2 sm:m-3 p-5 rounded-lg backdrop-blur-md h-full`}
+        } fixed sm:top-2 right-2 z-40 ease-in-out duration-300 sm:w-[95vw] md:w-[50vw] lg:w-[30vw] m-2 sm:m-3 p-5 rounded-lg backdrop-blur h-full`}
       >
         <div className="mb-2 flex items-center justify-between">
           <Typography variant="h5" color="white">
