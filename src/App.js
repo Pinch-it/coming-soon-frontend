1cbc88c487c593f095a654cd4c6f309939ae31a1
Syed Ridwan Ahmad <81156501+Ridwan6947@users.noreply.github.com>
2023-07-21 22:22:58 +0530

    changed the dimentions of whole website

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App  h-[1000vh]   ">
       <div className=' fixed  text-center  w-[100vw] p-[20px]'>
       <Navbar/>
-       <h3 className='text-white ff text-center mt-[5px] '>COMING SOON</h3>
+       <h3 className='text-white ff text-center mt-[3vh] '>COMING SOON</h3>
        <EmailForm/>
        <br/>
         <div className="text-center jj text-gray-500 text-[25px] font-bold">Countdown to Digital Awesomeness: Brace Yourself for the Unveiling!</div>
